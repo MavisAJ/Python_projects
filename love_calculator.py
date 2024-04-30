6341dfced40fb2938d7e644ee73fc27e654c5df5
Mavis Dorkenu <105258546+MavisAJ@users.noreply.github.com>
2024-04-30 20:16:02 +0000

    Update love_calculator.py

--- love_calculator.py
+++ love_calculator.py
@@ -1,9 +1,12 @@
 print('Welcome to the Love Calculator!')
+#getting name of the user and crush
 name1 = input("What is your name : ").lower()
 name2 = input('What is the name of your crush: ').lower()
-
+#concate their names
 combined_names = name1 + name2
 print(combined_names)
+
+# finding the characters of true love in the names combined
 count = 0
 count += combined_names.count('t')
 count += combined_names.count('r')
@@ -18,12 +21,13 @@ count_1 +=  combined_names.count('v')
 count_1 += combined_names.count('e')
 print(count_1)
 
-#Love calculaor
+# adding the count of the letters in true love in the combine names and printing out
 love_cal = str(count) + str(count_1)
 print(love_cal)
 love_calculator = int(love_cal)
 print(love_calculator)
 
+#Print out what the user sees with conditions based on their scores
 if love_calculator < 20 or  love_calculator> 90:
     print('Oops! Its unfortunate '+ name1.capitalize()+',' + ' You and your crush ' + name2.capitalize() + ' are not match afterall')
     print(f'Here is your score : {love_calculator}%')
@@ -32,6 +36,16 @@ elif love_calculator >= 40 and love_calculator<= 50:
 else:
     print(f'Your love score with your crush is {love_calculator}%')
     print('Give it a try!')
+
+
+
+
+
+
+
+
+
+
 
 
 
